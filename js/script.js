a3d0049b16622a0b40053a20ddb157c966d66957
Serdar <serdarcholukov@yandex.com>
2022-12-24 13:47:08 +0500

    22

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// import { a, b} from "./vars.js";
-// console.log(a,b);
 
 // All answer option 
 const option1 = document.querySelector('.option1'),
@@ -8,7 +6,7 @@ const option1 = document.querySelector('.option1'),
       option4 = document.querySelector('.option4');
 
 // All our options
-const optionElement = document.querySelector('.option');
+const optionElements = document.querySelectorAll('.option');
 
 const question = document.getElementById('question'),
       numberOfQuestion = document.getElementById("number-of-question"),
@@ -17,8 +15,8 @@ const question = document.getElementById('question'),
 let indexOfQuestion, //индекс текущего вопроса
     indexOfPage = 0; // индекс страниц
 
-    const answersTracker = document.querySelector('.answers-tracker');
-    const btnNext = document.querySelector('.btn-next');
+    const answersTracker = document.getElementById('answers-tracker');
+    const btnNext = document.getElementById('btn-next');
     
     let score = 0; // Итоговый результат викторины
 
@@ -47,15 +45,108 @@ const questions = [
       ],
       rightAnswer: 1
    },
+   {
+      question: "Как в JavaScript вычислить процент от числа 3333",
+      options: [
+         "Так в JavaScript нельзя делать 222",
+         "Оператор : % 222",
+         "Умножить на кол во процентов и разделить на 100 222",
+         "Вызвать метод findPrecent() 222",
+      ],
+      rightAnswer: 1
+   },
 ];
 
 
 numberOfAllQuestion.innerHTML = questions.length //выводим кол во всех вопросов
 
 const load = () => {
-   question.innerHTML = questions[1].question;
+   question.innerHTML = questions[indexOfQuestion].question; // сам вопрос
+
+   option1.innerHTML = questions[indexOfQuestion].options[0];
+   option2.innerHTML = questions[indexOfQuestion].options[1];
+   option3.innerHTML = questions[indexOfQuestion].options[2];
+   option4.innerHTML = questions[indexOfQuestion].options[3];
+
+   numberOfQuestion.innerHTML = indexOfPage + 1; // установка номера текущей страницы
+   indexOfPage++; // увеличение индекса страницы 
+
+}
+
+let completedAnswers = [];
+
+const randomQuestion = () => {
+   let randomNumber = Math.floor(Math.random() * questions.length);
+   let hitDublicate = false;
+
+   if(indexOfPage == questions.length) {
+      quizOver();
+   } else {
+      if(completedAnswers.length > 0) {
+         completedAnswers.forEach(item => {
+            if(item == randomNumber) {
+               hitDublicate = true;
+            }
+         });
+         if(hitDublicate) {
+            randomQuestion();
+         } else {
+            indexOfQuestion = randomNumber ;
+            load();
+         }
+      };
+      if(completedAnswers == 0) {
+         indexOfQuestion = randomNumber;
+         load();
+      }
+   };
+   completedAnswers.push(indexOfQuestion);
+};
+
+const checkAnswer = el => {
+
+   console.log(el.target.dataset.id);
+   if(el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
+      el.target.classList.add("correct");
+      score++;
+   } else {
+      el.target.classList.add("wrong");
+   }
+   disabledOptoins();
+}
+
+const disabledOptoins = () => {
+   optionElements.forEach(item => {
+      item.classList.add("disabled");
+      if(item.dataset.id == questions[indexOfQuestion].rightAnswer) {
+         item.classList.add("correct");
+      }
+   })
+}
+
+const enableOptions = () => {
+   optionElements.forEach(item => {
+      item.classList.remove("disabled","correct","wrong");
+   })
+};
+
+const answerTracker = () => {
+   questions.forEach(() => {
+      const div = document.createElement("div");
+      answersTracker.appendChild(div);
+   })
+};
+
+optionElements.forEach(option => {
+   option.addEventListener("click", e => checkAnswer(e));
+})
+
+const quizOver = () => {
+   console.log("over");
 }
 
 window.addEventListener("load",()=>{
-   load();
+   randomQuestion();
+   answerTracker();
 })
+
